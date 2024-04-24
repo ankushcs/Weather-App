fc5f8a85410c8eb83516546e7815f079cb2c4dba
ankushcs <ankushcswork@gmail.com>
2024-04-24 13:28:18 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,36 +1,26 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Weather App
 
-## Getting Started
+## Description
 
-First, run the development server:
+The Weather App is a web application built using Next.js and various other packages to provide real-time weather information. This app allows users to view current weather conditions, including temperature, humidity, wind speed, and more.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+## Packages Used
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- Next.js: A React framework for building server-side rendered and statically generated web applications.
+- Tailwind CSS: A utility-first CSS framework for quickly building custom designs.
+- TypeScript: A typed superset of JavaScript that compiles to plain JavaScript.
+- React Icons: A collection of popular icons as React components.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## Features
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+- View current weather conditions based on user's location or search query.
+- Display temperature, humidity, wind speed, and other relevant weather data.
+- Clean and responsive user interface built with Tailwind CSS.
+- Utilizes TypeScript for improved type safety and developer experience.
 
-## Learn More
+## How to Use
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+1. Clone the repository to your local machine.
+2. Install dependencies by running `npm install` or `yarn install`.
+3. Start the development server with `npm run dev` or `yarn dev`.
+4. Open your browser and navigate to `http://localhost:3000` to view the app.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,339 @@
-import Image from "next/image";
+"use client";
+import React, {
+  useState,
+  useEffect,
+  ChangeEventHandler,
+  MouseEventHandler,
+} from "react";
+import "./home.scss";
+import { MagnifyingGlass } from "react-loader-spinner";
+import { CiSearch } from "react-icons/ci";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { BsClouds, BsFillSunriseFill, BsFillSunsetFill } from "react-icons/bs";
+import { LiaTemperatureLowSolid } from "react-icons/lia";
+import { WiHumidity } from "react-icons/wi";
+import { MdOutlineVisibility, MdWindPower } from "react-icons/md";
+import { TiWeatherWindy } from "react-icons/ti";
+import { FaWind } from "react-icons/fa6";
+import { TbFaceIdError } from "react-icons/tb";
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+interface Location {
+  latitude: number | null;
+  longitude: number | null;
+}
+
+interface Error {
+  message: string;
+}
+
+interface WeatherData {
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  weather: [
+    {
+      main: string;
+      description: string;
+    }
+  ];
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+  };
+  name: string;
+  wind: {
+    deg: number;
+    speed: number;
+  };
+  visibility: number;
+  clouds: {
+    all: number;
+  };
+  sys: {
+    sunrise: number;
+    sunset: number;
+  };
+}
+
+const getLocationAsync = () => {
+  return new Promise<GeolocationPosition>((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const apiKey: string = "4d502482481442794edb3e852afae396";
+
+const Home: React.FC = () => {
+  const [location, setLocation] = useState<Location>({
+    latitude: null,
+    longitude: null,
+  });
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [inputLocation, setInputLocation] = useState<string>("");
+  const [fetchData, setFetchData] = useState<WeatherData | null>(null);
+  const [currentDateTime, setCurrentDateTime] = useState("");
+
+  const getCurrentDateTime = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1; // Month is zero-indexed
+    const day = now.getDate();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    // Formatting date components to ensure they have two digits
+    const formattedMonth = month < 10 ? `0${month}` : month;
+    const formattedDay = day < 10 ? `0${day}` : day;
+
+    // Formatting time components to ensure they have two digits
+    const formattedHours = hours < 10 ? `0${hours}` : hours;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    // Constructing the date and time strings
+    const currentDate = `${year}-${formattedMonth}-${formattedDay}`;
+    const currentTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    const currentDateTimeString = `${currentDate} ${currentTime}`;
+
+    return currentDateTimeString;
+  };
+
+  const fetchWeatherdata: () => void = async () => {
+    setLoading(true);
+    let url;
+    if (inputLocation.trim() !== "") {
+      url = `https://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&appid=${apiKey}`;
+    } else if (location.latitude !== null && location.longitude !== null) {
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${apiKey}`;
+    }
+    if (url) {
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          setFetchData(data);
+          setError(null);
+        } else {
+          setError({ message: response.statusText });
+        }
+      } catch (error) {
+        setError({ message: "An error occurred while fetching weather data." });
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  // for fetching the location by default -> as soon as the app will load this will work
+  const getLocation: () => void = () => {
+    setLoading(true);
+    getLocationAsync()
+      .then((position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+        setError(null);
+      })
+      .catch((error) => {
+        setError({ message: error.message });
+        setLocation({ latitude: null, longitude: null });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const renderLoader: () => React.JSX.Element = () => {
+    return (
+      <div>
+        <MagnifyingGlass
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="magnifying-glass-loading"
+          wrapperStyle={{}}
+          wrapperClass="magnifying-glass-wrapper"
+          glassColor="#c0efff"
+          color="black"
         />
       </div>
+    );
+  };
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  const handleOnChangeLocation: ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setInputLocation(event.target.value);
+  };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  const renderInput: () => React.JSX.Element = () => {
+    return (
+      <input
+        placeholder={fetchData?.name ? fetchData.name : "Enter your city"}
+        value={inputLocation}
+        onChange={handleOnChangeLocation}
+        className="bg-transparent pl-2 border-none outline-none"
+      />
+    );
+  };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  const handleInputLocation: MouseEventHandler<HTMLButtonElement> = () => {
+    fetchWeatherdata();
+  };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  // for automatically loading the location of the user
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  useEffect(() => {
+    // Fetch weather data whenever location changes
+    if (location.latitude !== null || location.longitude !== null) {
+      fetchWeatherdata();
+    }
+  }, [location]);
+
+  // Update the current date and time every second
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDateTime(getCurrentDateTime());
+    }, 1000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const renderSearchAndTimebar: () => React.JSX.Element = () => {
+    return (
+      <div className="flex justify-center md:justify-between aline-center items-center">
+        <div className="bg-white h-8 w-60 border-none outline-none flex justify-between items-center pr-0.5">
+          {renderInput()}
+          <button
+            onClick={handleInputLocation}
+            className="bg-gradient-to-t from-cyan-500 to-blue-800 h-7 w-7 flex justify-center items-center"
+          >
+            <CiSearch size={20} color="white" />
+          </button>
+        </div>
+        <div className="hidden md:block">
+          <div className="bg-white h-8 w-40 border-none outline-none flex justify-center items-center pr-0.5  ">
+            <p>{currentDateTime}</p>
+          </div>
+        </div>
       </div>
-    </main>
+    );
+  };
+
+  const getTemp = (temp: number | undefined) => {
+    if (temp) {
+      const result = temp - 273.15;
+      return parseFloat(result.toFixed(2));
+    }
+  };
+
+  const getWeatherDetails: () => React.JSX.Element = () => {
+    const weatherDetailsArray = [
+      {
+        id: 1,
+        title: "Clouds",
+        icon: <BsClouds size={25} />,
+        description: `${fetchData?.clouds.all} %`,
+      },
+      {
+        id: 2,
+        title: "Weather Type",
+        icon: <TiWeatherWindy size={25} />,
+        description: `${fetchData?.weather[0].main}`,
+      },
+      {
+        id: 3,
+        title: "Feels Like",
+        icon: <LiaTemperatureLowSolid size={25} />,
+        description: `${getTemp(fetchData?.main.feels_like)}\u00B0C`,
+      },
+      {
+        id: 4,
+        title: "Humidity",
+        icon: <WiHumidity size={25} />,
+        description: `${fetchData?.main.humidity} %`,
+      },
+      {
+        id: 5,
+        title: "Visibility",
+        icon: <MdOutlineVisibility size={25} />,
+        description: `${fetchData?.visibility} m`,
+      },
+      {
+        id: 6,
+        title: "Wind Speed",
+        icon: <FaWind size={25} />,
+        description: `${fetchData?.wind.speed} m/s`,
+      },
+      {
+        id: 7,
+        title: "Wind Direction",
+        icon: <MdWindPower size={25} />,
+        description: `${fetchData?.wind.deg}\u00B0`,
+      },
+    ];
+
+    return (
+      <ul className="flex justify-center flex-wrap gap-3 mt-10">
+        {weatherDetailsArray.map((weather) => (
+          <li
+            key={weather.id}
+            className="text-neutral-700 border border-neutral-100 bg-gray-400 bg-opacity-20 p-2 w-32 flex flex-col justify-center items-center gap-3 hover:scale-105 transition-all"
+          >
+            <p>{weather.title}</p>
+            <span>{weather.icon}</span>
+            <p>{weather.description}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderDisplayData = () => {
+    return (
+      <div className="text-center">
+        <p className="text-2xl text-neutral-700 mb-8">{fetchData?.name}</p>
+        <p className="text-7xl md:text-9xl current-weather text-white">
+          {getTemp(fetchData?.main.temp)}&deg;C
+        </p>
+        {getWeatherDetails()}
+      </div>
+    );
+  };
+
+  return (
+    <div className="min-h-screen md:h-screen w-screen bg-gradient-to-t from-cyan-500 to-blue-800 p-4 md:p-10">
+      <div className="border-2 border-neutral-500 h-full w-full bg-gray-400 bg-opacity-20 pt-10 pb-10">
+        {renderSearchAndTimebar()}
+        <div className="flex justify-center items-center h-full w-full mt-10">
+          {loading && renderLoader()}
+          {!loading && !error && fetchData?.coord.lon && renderDisplayData()}
+          {!loading && error && (
+            <div className="text-white flex justify-center items-center flex-col">
+              <TbFaceIdError size={50} />
+              <p>Error: {error.message}</p>
+              <p>Please search another City</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
